b748d7de625bdeb5ab21983c9b0861562d435101
CheolYong <cjfdyd4257@gmail.com>
2023-04-22 10:07:07 +0900

    MainView, MainViewModel 추가

--- WpfDINavigation/WpfDINavigation/App.xaml.cs
+++ WpfDINavigation/WpfDINavigation/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfDINavigation.ViewModels;
+using WpfDINavigation.Views;
 
 namespace WpfDINavigation
 {
@@ -13,5 +16,32 @@ namespace WpfDINavigation
     /// </summary>
     public partial class App : Application
     {
+        public new static App Current => (App)Application.Current;
+
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            // Views
+            services.AddSingleton(s => new MainView() 
+            { 
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+
+            return services.BuildServiceProvider();
+        }
+
+        public App()
+        {
+            Services = ConfigureServices();
+
+            var mainView = Services.GetRequiredService<MainView>();
+            mainView.Show();
+        }
+
+        public IServiceProvider Services { get; }
     }
 }

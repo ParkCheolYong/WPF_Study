6d74045365ecdbfd40b7b93b91cc4f304d796b49
박철용 <cjfdyd4257@gmail.com>
2023-04-27 13:48:24 +0900

    Updates

--- WpfDINavigation/WpfDINavigation/ViewModels/TestControlViewModels/RightViewModel.cs
+++ WpfDINavigation/WpfDINavigation/ViewModels/TestControlViewModels/RightViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using WpfDINavigation.Commands;
 using WpfDINavigation.Models;
 using WpfDINavigation.Stores;

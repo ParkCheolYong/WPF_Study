1254b70ad8428983f1ee85c5d0cf3cf19661dd47
박철용 <cjfdyd4257@gmail.com>
2023-05-04 15:31:13 +0900

    Add CustomDialog

---
+++ WpfDialogBox/WpfDialogBox/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDialogBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOkCancel_Click(object sender, RoutedEventArgs e)
+        {
+            var okCancelDialog = new OkCancelDialog("Title", "Message");
+            bool? result = okCancelDialog.ShowDialog();
+            MessageBox.Show(result.ToString());
+        }
+
+        private void btnMessageBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInputBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
